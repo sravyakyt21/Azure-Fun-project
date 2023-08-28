782e8855ff33d943e1ce135d4a9a6820b623d97b
yasheshgurrala <yasheshgurrala@gmail.com>
2023-08-27 22:58:11 -0500

    azure func test commit

---
+++ func-test/__init__.py
@@ -0,0 +1,26 @@
+import logging
+
+import azure.functions as func
+
+a = 1
+b = 2 
+c = a + b
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             f"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. The sum of a + b = {c}",
+             status_code=200
+        )
